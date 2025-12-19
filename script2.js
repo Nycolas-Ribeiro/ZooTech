3695254d85513ceb39ee6429a08f32a48cdbddbc
Nycolas-Ribeiro <nycolasribeiro105@gmail.com>
2025-12-19 18:49:17 -0300

    Add click event listener to toggle response visibility

--- script2.js
+++ script2.js
@@ -1,1 +1,6 @@
-
+document.querySelectorAll(".pergunta").forEach(pergunta => {
+            pergunta.addEventListener("click", () => {
+                pergunta.querySelector(".resposta").classList.toggle("abrir")
+                
+            })
+        })
